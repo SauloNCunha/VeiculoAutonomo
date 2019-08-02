4c20b896d6035f167c7ec066fa6be6d6616a3fda
valtermjr93 <valtermjr@gmail.com>
2019-08-01 21:34:15 -0300

    Update acionado_com_shild.ino

--- acionado_com_shild/acionado_com_shild.ino
+++ acionado_com_shild/acionado_com_shild.ino
@@ -1,23 +1,30 @@
+#include <Servo.h>
 
-//#include <Servo.h>
-//#include <SharpIR.h>
 
-const int analogInPin = A0;
 
 #define velDiant 5 //portas definidas pela shild para os motores (padrao)
 #define dirDiant 0 //portas definidas pela shild para os motores (padrao) 
 #define velTras  4 //portas definidas pela shild para os motores (padrao)
 #define dirTras  2 //portas definidas pela shild para os motores (padrao)
 
-#define model 1080 //DETERMINA O MODELO DO SENSOR (1080 PARA O MODELO 2Y0A21Y OU 20150 PARA O MODELO 2Y0A02Y)
+#define TRIGGERdir 12
+#define ECHOdir    13
 
-//SharpIR SharpIR(analogInPin, model); //PASSA OS PARÂMETROS PARA A FUNÇÃO (PINO ANALÓGICO, MODELO DO SENSOR)
+//#define TRIGGERdiresq 16
+//#define ECHOdiresq    17
 
-//Servo servo;
+//#define TRIGGERdirfre 5
+//#define ECHOdirfre    4
+
+float dir = 0;
+//float esq = 0;
+//float fre = 0;
+
+Servo servo;
 
 int sensorValue = 0;
 int cmValue = 0;
-int sensorPin = A0;
+
 
 int minRange = 512;
 int maxRange = 512;
@@ -30,20 +37,75 @@ int pos = 0;
 void setup(){
   Serial.begin(9600);
    
-  /*// initial settings for motors off and direction forward
-  servo.attach(13); // servo está na porta 13
+  // initial settings for motors off and direction forward
+  servo.attach(16); // servo está na porta 15
   servo.write(pos); // aciona a porta do servo, zerando
   pinMode(velDiant, OUTPUT); //declara saida dos pinos
   pinMode(velTras, OUTPUT);
   pinMode(dirDiant, OUTPUT);
   pinMode(dirTras, OUTPUT);
-
   digitalWrite(velDiant, LOW); //declara motores desligados
   digitalWrite(velTras, LOW);
   digitalWrite(dirDiant, LOW);
   digitalWrite(dirTras,LOW);
-  */
-  pinMode(sensorPin, INPUT);
+  
+
+  
+  pinMode(TRIGGERdir, OUTPUT);
+  pinMode(ECHOdir, INPUT);
+}
+
+void distancia() {
+  
+  long duration, distance;
+  digitalWrite(TRIGGERdir, LOW);  
+  delayMicroseconds(2); 
+  
+  digitalWrite(TRIGGERdir, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(TRIGGERdir, LOW);
+  duration = pulseIn(ECHOdir, HIGH);
+  dir = (duration/2) / 29.1;
+  Serial.println(duration); 
+
+  //-------------------------------
+
+  /*long duration, distance;
+  digitalWrite(TRIGGERdir, LOW);  
+  delayMicroseconds(2); 
+  
+  digitalWrite(TRIGGERdir, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(TRIGGERdir, LOW);
+  duration = pulseIn(ECHOdir, HIGH);
+  esq = (duration/2) / 29.1;*/
+
+  //----------------------------------
+
+  /*long duration, distance;
+  digitalWrite(TRIGGERdir, LOW);  
+  delayMicroseconds(2); 
+  
+  digitalWrite(TRIGGERdir, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(TRIGGERdir, LOW);
+  duration = pulseIn(ECHOdir, HIGH);
+  fre = (duration/2) / 29.1;*/
+  
+  
+  Serial.print("Centimeter:");
+  Serial.println(dir);
+  delay(1000);
+}
+
+void pontomorto(){
+  digitalWrite(velDiant, LOW);
+  digitalWrite(velTras, LOW);
+  digitalWrite(dirDiant, LOW);
+  digitalWrite(dirTras,LOW);
 }
 
 void frente(){
@@ -60,19 +122,52 @@ void re(){
   analogWrite(velTras,maxSpeed);
 }
 
+void esquerda(){
+  int angulo = dir;
+  for (angulo; angulo < 360; angulo += 1) { // Comando que muda a posição do servo de 0 para 180°
+  servo.write(angulo); // Comando para angulo específico
+  delay(15); 
+  }
+  
+  delay(2000); // Tempo de espera para próximo movimento
+  for (angulo = 360; angulo >= 1; angulo -= 5) { // Comando que muda a posição do servo de 180 para 0°
+  servo.write(angulo); // Comando para angulo específico
+  delay(5);
+  }
+  
+delay(2000);
+  
+  /*// Lê o valor do Potenciometro
+  int angle = dir; 
+  // Mapeia o valor de 0 a 180 graus
+  angle=map(angle, 0, 1023, 0, 180);
+  // Repassa o angulo ao ServoWrite
+  servo.write(angle); 
+  // Delay de 15ms para o Servo alcançar a posição
+  delay(15);*/
+}
+
 void loop(){
-  /*
-  frente();
-  delay(1000);
+  distancia();
+
+  /*frente();
+  delay(2000);
   
   re();
-  delay(1000);
-  */
+  delay(2000);
 
-  sensorValue = analogRead(sensorPin);
-  cmValue = 27.728 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.2045);
-  Serial.print("Valor lido = ");
-  Serial.print(cmValue);
-  Serial.println(" cm");
-  delay(250);
+  pontomorto();
+  delay(2000);*/
+  //esquerda();
+  if(dir <= 10 ) {
+    pontomorto();
+  }
+  else{
+  frente();
+  
+  //re();
+  }
+  
+  Serial.print("distancia: "); 
+  Serial.println(dir);
 }
